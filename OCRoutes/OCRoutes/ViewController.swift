0998684741d6675efabc2b1989cefa2097346687
Brandon Danis <bdani093@uottawa.ca>
2018-01-30 00:11:33 -0500

    Basic setup to test MapKit

--- OCRoutes/OCRoutes/ViewController.swift
+++ OCRoutes/OCRoutes/ViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    var mapView : MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .white
+        
+        mapView = MKMapView(frame: CGRect.zero)
+        
+        view.addSubview(mapView)
+        
+        SetupContraint()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    private func SetupContraint() {
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.topAnchor),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
 
 }
 
