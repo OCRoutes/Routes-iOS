da58d1a7934dbeb36de98228a9ca949fe1967ff6
Brandon Danis <bdani093@uottawa.ca>
2018-02-03 14:18:00 -0500

    Added redLine to cell

--- OCRoutes/OCRoutes/Custom Elements/FavoriteStopTableViewCell.swift
+++ OCRoutes/OCRoutes/Custom Elements/FavoriteStopTableViewCell.swift
@@ -11,6 +11,12 @@ import UIKit
 
 class FavoriteStopTableViewCell : UITableViewCell {
     
+    let redLineView : UIView = {
+        let view = UIView()
+        view.backgroundColor = Style.mainColor
+        return view
+    }()
+    
     let busStopNumberLabel : UILabel = {
         let label = UILabel()
         label.text = "6783"
@@ -19,11 +25,21 @@ class FavoriteStopTableViewCell : UITableViewCell {
         return label
     }()
     
+    let busStopNameLabel : UILabel = {
+        let label = UILabel()
+        label.text = "King Edward / Tempelton"
+        label.font = UIFont(name: "AvenirNext", size: 18)
+        label.textColor = Style.darkGrey
+        return label
+    }()
+    
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
         //Adding elements to contentView
+        contentView.addSubview(redLineView)
         contentView.addSubview(busStopNumberLabel)
+        contentView.addSubview(busStopNameLabel)
         
         ApplyConstraints()
     }
@@ -34,13 +50,26 @@ class FavoriteStopTableViewCell : UITableViewCell {
     
     private func ApplyConstraints() {
         
+        redLineView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            redLineView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            redLineView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            redLineView.leftAnchor.constraint(equalTo: contentView.leftAnchor),
+            redLineView.widthAnchor.constraint(equalToConstant: 2)
+        ])
+        
         busStopNumberLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             busStopNumberLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
-            busStopNumberLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 15)
+            busStopNumberLabel.leftAnchor.constraint(equalTo: redLineView.rightAnchor, constant: 15)
         ])
         
-        
+        busStopNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            busStopNameLabel.leftAnchor.constraint(equalTo: busStopNumberLabel.rightAnchor, constant: 10),
+            busStopNameLabel.bottomAnchor.constraint(equalTo: busStopNumberLabel.bottomAnchor),
+            busStopNameLabel.heightAnchor.constraint(equalTo: busStopNumberLabel.heightAnchor)
+        ])
         
     }
     
